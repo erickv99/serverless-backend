9e22ce2b9c01092eea57796618e03f9d2e7ec192
erick.valdivia <erick.valdivia@brightcove.com>
2024-04-01 22:46:34 -0400

    added seeds

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ https://ej64rpzdqi.execute-api.us-east-1.amazonaws.com/dev/products
 https://ej64rpzdqi.execute-api.us-east-1.amazonaws.com/dev/products/{id}
 
 https://ej64rpzdqi.execute-api.us-east-1.amazonaws.com/dev/products/1
+
+# Module 4: Integration with NoSQL Database
+
+## Task 4.1
+
+Inside /seed folder
+
+```
+products.sh
+```
+
+```
+stocks.sh
+```
---
+++ seed/products.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws dynamodb put-item --table-name Products --item '{"id":{"S":"1dfea959-8b9b-428e-9ee1-3f2e95dfa979"}, "title":{"S":"Vase"},"description":{"S":"white vase"}, "price":{"N":"25"}}'
+aws dynamodb put-item --table-name Products --item '{"id":{"S":"1dfea959-8b9b-428e-9ee1-3f2e95dfa980"}, "title":{"S":"Chair"},"description":{"S":"wooden chair"}, "price":{"N":"60"}}'
+aws dynamodb put-item --table-name Products --item '{"id":{"S":"1dfea959-8b9b-428e-9ee1-3f2e95dfa981"}, "title":{"S":"Lamp"},"description":{"S":"bedside lamp"}, "price":{"N":"30"}}'
+aws dynamodb put-item --table-name Products --item '{"id":{"S":"1dfea959-8b9b-428e-9ee1-3f2e95dfa982"}, "title":{"S":"Table"},"description":{"S":"round table"}, "price":{"N":"120"}}'
+aws dynamodb put-item --table-name Products --item '{"id":{"S":"1dfea959-8b9b-428e-9ee1-3f2e95dfa983"}, "title":{"S":"Carpet"},"description":{"S":"blue carpet"}, "price":{"N":"45"}}'

0cecccfd8a2868599f25a6974e96c7fdffd644fc
erick.valdivia <erick.valdivia@brightcove.com>
2024-04-05 18:47:11 -0400

    added copy to parsed folder

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -1,5 +1,10 @@
 import { S3Event } from 'aws-lambda';
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { parse } from 'csv-parse';
 const s3Client = new S3Client({
   region: process.env.REGION,
@@ -12,6 +17,7 @@ const importFileParser = async (event: S3Event) => {
     const fileName = decodeURIComponent(record.s3.object?.key);
     console.log(`File uploaded: ${fileName}`);
 
+    // Parse and log
     const s3File = await s3Client.send(
       new GetObjectCommand({
         Bucket: process.env.BUCKET_NAME,
@@ -30,6 +36,24 @@ const importFileParser = async (event: S3Event) => {
 
       console.log('result', data);
     });
+
+    // Copy
+    const fileNameWithNewFolder = fileName.replace('uploaded/', 'parsed/');
+    const command = new CopyObjectCommand({
+      CopySource: `${process.env.BUCKET_NAME}/${fileName}`,
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileNameWithNewFolder,
+    });
+    await s3Client.send(command);
+    console.log('file copied to', fileNameWithNewFolder);
+
+    // Delete
+    const deleteCommand = new DeleteObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileName,
+    });
+    await s3Client.send(deleteCommand);
+    console.log('file removed', fileName);
   }
 };
 
